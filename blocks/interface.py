bcda599600a79cc4858aa9292b32a446d6d5a4d5
Cry-StalL <940755868@qq.com>
2025-02-02 14:10:10 +0800

    暂时删除功能区切换相关代码

--- blocks/interface.py
+++ blocks/interface.py
@@ -1,11 +1,5 @@
 import gradio as gr
 
-from blocks.toolbox.code_generation import GenerateFromDescription, CodeCompletion
-from blocks.toolbox.code_explanation import GenerateCodeDocumentation, GenerateCodeComments
-from blocks.toolbox.code_enhancement import BugFixing, CodeOptimization
-from blocks.toolbox.code_testing import TestCaseGeneration
-# from blocks.toolbox import *
-
 NAV_ITEMS = {
     "📝 代码生成": ["从描述生成", "代码补全"],
     "🔍 代码解释": ["生成代码说明", "生成代码注释"],
@@ -15,8 +9,6 @@ NAV_ITEMS = {
 
 def interface():
     with gr.Blocks() as block:
-        mode = gr.State("None") # 初始状态
-
         with gr.Row():
             with gr.Column(scale=1, min_width=245, variant="compact"):
                 # left panel (navigation bar)
@@ -42,37 +34,15 @@ def interface():
             with gr.Column():
                 gr.Markdown("### 🔧 功能区")
 
-                @gr.render(inputs=mode)
-                def render_toolbox(mode):
-                    print(mode)
-                    if mode == "从描述生成":
-                        GenerateFromDescription.get_block()
-                    elif mode == "代码补全":
-                        CodeCompletion.get_block()
-                    elif mode == "生成代码说明":
-                        GenerateCodeDocumentation.get_block()
-                    elif mode == "生成代码注释":
-                        GenerateCodeComments.get_block()
-                    elif mode == "错误修复":
-                        BugFixing.get_block()
-                    elif mode == "代码优化":
-                        CodeOptimization.get_block()
-                    elif mode == "测试用例生成及在线测试":
-                        TestCaseGeneration.get_block()
-
         for radio in radio_components:
             radio.select(
-                fn=handle_nav_selection_for_radios,
+                fn=handle_nav_selection,
                 inputs=radio,
                 outputs=radio_components,
             )
-            radio.select(
-                fn=lambda x: x,
-                inputs=radio,
-                outputs=mode,
-            )
 
-def handle_nav_selection_for_radios(selected_item): # 导航栏按钮选中事件的handler
+
+def handle_nav_selection(selected_item): # 导航栏按钮选中事件的handler
     """处理导航选择事件：选中一个时自动取消其他分类的选择"""
     radio_components_update = []
 
--- blocks/toolbox/code_explanation/GenerateCodeComments.py
+++
@@ -1,7 +0,0 @@
-import gradio as gr
-
-def get_block():
-    with gr.Blocks() as block:
-        gr.Markdown("### generate_code_comments功能区(待编写)")
-
-    return block
