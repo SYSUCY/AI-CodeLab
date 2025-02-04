3e3896e0135c6732ed42692ba3759378471027e9
Cry-StalL <940755868@qq.com>
2025-02-04 11:27:56 +0800

    主界面编程语言选择功能

--- blocks/interface.py
+++ blocks/interface.py
@@ -7,6 +7,44 @@ NAV_ITEMS = {
     "✅ 代码测试": ["测试用例生成"]
 }
 
+# 下面包含了Gradio中Code组件支持的所有语言
+lang_map = {
+    # 通用编程语言
+    'Python': 'python',
+    'C': 'c',
+    'C++': 'cpp',
+    'R': 'r',
+
+    # 标记语言/数据格式
+    'Markdown': 'markdown',
+    'JSON': 'json',
+    'HTML': 'html',
+    'CSS': 'css',
+    'YAML': 'yaml',
+    'Dockerfile': 'dockerfile',
+
+    # 脚本语言
+    'JavaScript': 'javascript',
+    'TypeScript': 'typescript',
+    'Shell': 'shell',
+    'Jinja2': 'jinja2',
+
+    # SQL及其方言
+    'SQL': 'sql',
+    'Microsoft SQL': 'sql-msSQL',
+    'MySQL': 'sql-mySQL',
+    'MariaDB': 'sql-mariaDB',
+    'SQLite': 'sql-sqlite',
+    'Cassandra Query Language (CQL)': 'sql-cassandra',
+    'PL/SQL': 'sql-plSQL',
+    'HiveQL': 'sql-hive',
+    'PL/pgSQL': 'sql-pgSQL',
+    'GraphQL': 'sql-gql',
+    'Greenplum SQL': 'sql-gpSQL',
+    'Spark SQL': 'sql-sparkSQL',
+    'Esper EPL': 'sql-esper'
+}
+
 def interface():
     with gr.Blocks() as block:
         with gr.Row():
@@ -29,10 +67,10 @@ def interface():
 
             with gr.Column(scale=9, min_width=800):
                 with gr.Row():
-                    lang_selector = gr.Dropdown(label="请选择编程语言")
+                    lang_selector = gr.Dropdown(label="请选择编程语言", choices=list(lang_map.keys()) , interactive=True, filterable=True, value=None)
                     model_selector = gr.Dropdown(label="请选择使用的模型")
                 # code editor
-                gr.Code(lines=30, max_lines=30)
+                editor = gr.Code(lines=30, max_lines=30, interactive=True)
         with gr.Row():
             # toolbox
             with gr.Column():
@@ -45,6 +83,11 @@ def interface():
                 outputs=radio_components,
             )
 
+        lang_selector.change(
+            fn=handle_lang_selection,
+            inputs=lang_selector,
+            outputs=editor,
+        )
 
 def handle_nav_selection(selected_item): # 导航栏按钮选中事件的handler
     """处理导航选择事件：选中一个时自动取消其他分类的选择"""
@@ -58,3 +101,6 @@ def handle_nav_selection(selected_item): # 导航栏按钮选中事件的handler
 
 
     return radio_components_update
+
+def handle_lang_selection(selected_item):
+    return gr.update(language=lang_map[selected_item])
