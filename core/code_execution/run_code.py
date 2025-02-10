9ea0e643305410d5fbf5cd645db6ee9399cc15c1
Cry-StalL <940755868@qq.com>
2025-02-10 11:16:24 +0800

    使用自行搭建的API实现Python代码的在线运行

--- core/code_execution/run_code.py
+++ core/code_execution/run_code.py
@@ -1,14 +1,34 @@
-import subprocess
 import sys
+import os
+import requests
 
-def run_code(language, code):
-    if language == "Python":
-        output = run_python(code)
-    else:
-        print("Error: Unsupported language to execute")
-        return None
+CODE_RUNNER_IP = os.environ.get("AI_CODELAB_CODE_RUNNER_IP")
+if CODE_RUNNER_IP is None:
+    raise ValueError("Environment variable AI_CODELAB_CODE_RUNNER_IP is not set")
 
-    return output
+POST_URL = f"http://{CODE_RUNNER_IP}:8000/run_code"
 
 def run_python(code):
-    return "Test"
+    data = {"language": "Python", "code": code}
+
+    response = requests.post(POST_URL, json=data)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+language_map = {
+    "Python": run_python,
+}
+
+def run_code(language, code):
+    if language not in language_map:
+        raise ValueError(f"Language {language} is not supported")
+
+    try:
+        output = language_map[language](code)
+    except Exception as e:
+        return {"error": str(e)}
+
+    return output
